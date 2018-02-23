3583a21e9b97ced0a1ef8f9ab21fafc8124b5334
sivaprakash8580 <36755484+sivaprakash8580@users.noreply.github.com>
2018-02-23 16:21:10 +0530

    Create largest three number.py

---
+++ beginner/largest three number.py
@@ -0,0 +1,9 @@
+num1=input("enter the number1")
+num2=input("enter the number2")
+num3=input("enter the number3"
+if(num1>num2 and num1>num3):
+ print('num1 is greater')
+elif(num2>num1 and num2>num3):
+ print('num2 is greater')
+else:
+ print('num3 is greater')
