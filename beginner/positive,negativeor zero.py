105a9c777d8528050612bafb46bd22edd88ad092
sivaprakash8580 <36755484+sivaprakash8580@users.noreply.github.com>
2018-02-23 14:38:28 +0530

    Create positive,negativeor zero.py

---
+++ beginner/positive,negativeor zero.py
@@ -0,0 +1,9 @@
+n=float(input("enter the number"))
+if 1<=n<=100000:
+  print('the number is positive')
+elif -1<=n<=-100000:
+ print('the number is negative')
+elif o:
+ print('the number is zero')
+else:
+ print('the number is over limit')
